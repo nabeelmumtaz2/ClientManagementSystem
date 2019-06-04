b414714dfd37cebd7cd2fe825fbb8ec51e132fe2
Nabeel Mumtaz <Nabeel Mumtaz@NABEEL>
2019-06-04 18:58:47 -0400

    Added client controller

---
+++ ClientManagementSystem/Controllers/ClientsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClientManagementSystem;
+
+namespace ClientManagementSystem.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private ClientdbContainer db = new ClientdbContainer();
+
+        // GET: Clients
+        public ActionResult Index()
+        {
+            return View(db.Clients.ToList());
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clients clients = db.Clients.Find(id);
+            if (clients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clients);
+        }
+
+        // GET: Clients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,First_Name,Last_name,Address,Registration_date,Telephone,City,Postal_Code,Email,Comments,Owed_money,Domain,Gender,Status,Business_type,Business_name,Product_name,Picture_url,Country,Cell,How_much,Note")] Clients clients)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clients.Add(clients);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clients);
+        }
+
+        // GET: Clients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clients clients = db.Clients.Find(id);
+            if (clients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clients);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,First_Name,Last_name,Address,Registration_date,Telephone,City,Postal_Code,Email,Comments,Owed_money,Domain,Gender,Status,Business_type,Business_name,Product_name,Picture_url,Country,Cell,How_much,Note")] Clients clients)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clients).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clients);
+        }
+
+        // GET: Clients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clients clients = db.Clients.Find(id);
+            if (clients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clients);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Clients clients = db.Clients.Find(id);
+            db.Clients.Remove(clients);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
